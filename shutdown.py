ec66c1bd7a80f429c2a44dc7b6f8d769acc4b987
Daniel Martin <32564392+dmanlfc@users.noreply.github.com>
2018-12-08 15:23:05 +1000

    tidy up

--- shutdown.py
+++ shutdown.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
-import time
-import os
+from time import sleep
+from os import system
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
@@ -8,9 +8,7 @@ GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while True:
         buttonState = GPIO.input(5)
-
         if buttonState == True:
                 print("Switch was set to off!")
-                os.system("shutdown now -h")
-                time.sleep(1)
-
+                system("shutdown now -h")
+                sleep(1)
