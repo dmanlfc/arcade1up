33e15878299c502cd541578dc14dfc75b9f35c5c
Daniel Martin <32564392+dmanlfc@users.noreply.github.com>
2019-03-08 16:03:03 +1000

    model3.sh launch script

---
+++ Latte Panda/model3/model3.sh
@@ -0,0 +1,11 @@
+# this will launch the model3 emulator with specific settings
+# requires the compiled supermodel binary
+# the Config, NVRAM & Saves folders need to be in here too
+# make the script executable chmod +x
+
+ROM="$1"
+# ensure supermodel runs from the correct directory - fails otherwise
+cd /opt/retropie/emulators/model3
+
+# we use -legacy3d as the intel graphics card doesn't work well with the new 3d engine
+./supermodel -fullscreen -res=1280,1024 -legacy3d -multi-texture - quad-rendering "$ROM"
